cd7479d337bae83a1ee0e843f400a65595f6d285
zzyybs <zzyybs@126.com>
2019-09-10 17:17:12 +0800

    activemq for java0508

---
+++ src/main/java/com/atguigu/activemq/JMSConsumer.java
@@ -0,0 +1,73 @@
+package com.atguigu.activemq;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ * @auther zzyy
+ * @create 2019-09-10 14:51
+ */
+public class JMSConsumer
+{
+    public static final String MQ_URL = "tcp://192.168.111.142:61616";
+    public static final String MyQUEUE = "queue0508";
+
+    public static void main(String[] args) throws JMSException
+    {
+
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(MQ_URL);
+
+        Connection connection = activeMQConnectionFactory.createConnection();
+        connection.start();
+
+        Session session = connection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+
+        Queue queue = session.createQueue(MyQUEUE);
+        MessageConsumer messageConsumer = session.createConsumer(queue);
+
+
+       /*同步阻塞方式receive() ，订阅者或接收者调用MessageConsumer的receive()方法来接收消息，
+        receive()将一直阻塞
+        receive(long timeout) 按照给定的时间阻塞，到时间自动退出*/
+        TextMessage textMessage = null;
+        while(true)
+        {
+            textMessage = (TextMessage) messageConsumer.receive();
+
+            if(null != textMessage)
+            {
+                System.out.println("****收到的消息："+textMessage.getText());
+                textMessage.acknowledge();;
+                //session.commit();;
+            }else{
+                break;
+            }
+        }
+        messageConsumer.close();
+
+        session.close();
+        connection.close();
+
+
+
+       /*
+        异步非阻塞方式(监听器onMessage())
+        订阅者或接收者通过MessageConsumer的setMessageListener(MessageListener listener)注册一个消息监听器，
+        当消息到达之后，系统自动调用监听器MessageListener的onMessage(Message message)方法。
+        messageConsumer.setMessageListener((message) -> {
+            if(null != message && message instanceof TextMessage)
+            {
+                TextMessage textMessage = (TextMessage) message;
+                try {
+                    System.out.println("****收到的消息："+textMessage.getText());
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
+        });*/
+
+
+
+    }
+}
---
+++ src/main/java/com/atguigu/activemq/JMSProduce.java
@@ -0,0 +1,53 @@
+package com.atguigu.activemq;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ * @auther zzyy
+ * @create 2019-09-10 14:31
+ */
+public class JMSProduce
+{
+    public static final String MQ_URL = "tcp://192.168.111.142:61616";
+    public static final String MyQUEUE = "queue0508";
+
+    public static void main(String[] args) throws JMSException
+    {
+        //1 通过ConnectionFactory工厂
+        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory(MQ_URL);
+
+        //2 获得connection对象并启动
+        Connection connection = activeMQConnectionFactory.createConnection();
+        connection.start();
+
+        //3 通过connection对象获得session对象
+        // 第一个参数叫mq的事务/第二个参数叫消息的签收，此时忽略用默认
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        //4 通过session获得目的地
+        Queue queue = session.createQueue(MyQUEUE);
+        //5 通过session获得消息的生产者
+        MessageProducer messageProducer = session.createProducer(queue);
+        //messageProducer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+        //messageProducer.setDeliveryMode(DeliveryMode.PERSISTENT);
+        //6 开始生产3条消息发送到Activemq上
+        for (int i = 1; i <=5; i++)
+        {
+            TextMessage textMessage = session.createTextMessage("msg---" + i);
+            //7 用messageProducer发送消息到mq
+            messageProducer.send(textMessage);
+        }
+        //8 释放资源
+        messageProducer.close();
+        //session.commit();
+        session.close();
+        connection.close();
+
+        System.out.println("*****run is ok");
+
+
+
+    }
+}
